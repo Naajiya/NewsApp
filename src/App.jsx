5e19b1a429db1898521520b690aca65f5be5547a
NAJIYA KP <najiyabinthmajeed@gmail.com>
2024-11-24 08:35:01 -0800

    code updated

--- src/App.jsx
+++ src/App.jsx
@@ -15,9 +15,11 @@ function App() {
   const API_KEY = "d9524749a3c2409ea299f2fe98b02875"
   const [search, setSearch] = useState("india")
   const [news, setNews] = useState([])
-  const [topic, setTopic] = useState('')
+  const [color, setBgBtnColor] = useState()
+  const [activeBtn, setActiveBtn] = useState('')
 
 
+  console.log(activeBtn)
 
   const [show, setShow] = useState(false);
   const [activeNews, setActiveNews] = useState(null);
@@ -26,7 +28,7 @@ function App() {
     setActiveNews(null);
   };
   const handleShow = (newsItem) => {
-   
+
     setActiveNews(newsItem);
   };
 
@@ -36,28 +38,28 @@ function App() {
   // https://newsapi.org/v2/everything?q=${search}&apiKey=${API_KEY}
   // https://newsapi.org/v2/everything?q=sports&apiKey=${API_KEY}
 
-  useEffect(()=>{
+  useEffect(() => {
     getVideo()
-  },[])
+  }, [])
 
-  const getVideo=async()=>{
-    try{
+  const getVideo = async () => {
+    try {
       const { data } = await axios.get(`https://newsapi.org/v2/everything?q=${search}&apiKey=${API_KEY}`)
       setNews(data.articles)
-    }catch(err){
+    } catch (err) {
       console.log(err)
     }
   }
 
 
 
-  const handleNews = async () => {
+  const handleNews = async (to) => {
     try {
       const { data } = await axios.get(`https://newsapi.org/v2/everything?q=${search}&apiKey=${API_KEY}`)
       console.log(data)
       setNews(data.articles)
       setSearch('')
-
+      setActiveBtn(to)
     } catch (err) {
       console.log(err)
     }
@@ -67,19 +69,17 @@ function App() {
     try {
       const { data } = await axios.get(`https://newsapi.org/v2/everything?q=${to}&apiKey=${API_KEY}`)
       console.log(data.articles)
-     
+      // setBgBtnColor('blue')
       // if(data.articles.content != [Rwm])
       setNews(data.articles)
-      setColor()
+      setActiveBtn(to)
+
+
     } catch (err) {
       console.log(err)
     }
-  } 
-
-
-  const setColor=()=>{
-    style.button.color='red'
   }
+
 
 
   return (
@@ -90,15 +90,15 @@ function App() {
       <div className='container-fluid'>
         <Row>
           <Row>
-            <Navbar className=" text-light align-items-center justify-content-center p-3" style={{backgroundColor:'#89A8B2'}}>
+            <Navbar className="d-flex flex-column flex-md-row text-light text-center align-items-center justify-content-center p-3" style={{ backgroundColor: '#89A8B2' }}>
               <Col sm={12} lg={6}>
-                <Form >
-                  <h2 style={{ fontFamily: 'fantasy' }}>NewsLatest</h2>
-                </Form>
+                
+                  <h2 style={{ fontFamily: 'fantasy' }}>NewsApp</h2>
+                
               </Col>
-              <Form >
-                <Col >
-                  <Row inline>
+              <Form className="w-100 w-md-auto">
+                
+                  <Row inline className='d-flex flex-column flex-md-row p-3 align-items-center'>
                     <Col xs="auto">
                       <Form.Control
                         onChange={(e) => setSearch(e.target.value)}
@@ -108,25 +108,67 @@ function App() {
                       />
                     </Col>
                     <Col xs="auto">
-                      <Button onClick={handleNews}>Submit</Button>
+                     
+                        <Button className='m-2' onClick={()=>handleNews('search')}
+                        style={{
+                          backgroundColor: activeBtn == 'search' ? 'red' : ''
+                        }}>Submit</Button>
+                    
                     </Col>
                   </Row>
-                </Col>
+               
               </Form>
             </Navbar>
           </Row>
 
           <Row className='p-2'>
-            <Col lg={2} className='border border-3 border-dark p-2 ' style={{backgroundColor:'#B3C8CF'}}>
+            <Col lg={2} sm={12} className='border border-3 border-info rounded p-2 ' style={{ backgroundColor: '#B3C8CF' }}>
               <div className='d-flex flex-column align-items-center p-2'>
                 {/* <h4>Other News</h4> */}
-                <Button onClick={() => handleSports('health')} className='m-2 w-100' variant="outline-light">Health</Button>
-                <Button onClick={() => handleSports('sports')} className='m-2 w-100' variant="outline-dark">Sports</Button>
-                <Button onClick={() => handleSports('technology')} className='m-2 w-100' variant="outline-dark">Technology</Button>
-                <Button onClick={() => handleSports('business')} className='m-2 w-100' variant="outline-dark">Business</Button>
-                <Button onClick={() => handleSports('politics')} className='m-2 w-100' variant="outline-dark">Politics</Button>
-                <Button onClick={() => handleSports('entertainment')} className='m-2 w-100' variant="outline-dark">Entartainment</Button>
-                <Button onClick={() => handleSports('science')} className='m-2 w-100' variant="outline-dark">science</Button>
+                
+                <Button onClick={() => handleSports('health')} className='m-2 w-100' variant="outline-light"
+                  style={{
+                    backgroundColor: activeBtn == 'health' ? 'red' : ''
+                  }}>Health</Button>
+
+
+                <Button onClick={() => handleSports('sports')} className='m-2 w-100' variant="outline-dark"
+                  style={{
+                    backgroundColor: activeBtn == 'sports' ? 'red' : ''
+                  }}
+                >Sports</Button>
+
+
+                <Button onClick={() => handleSports('technology')} className='m-2 w-100' variant="outline-dark"
+                  style={{
+                    backgroundColor: activeBtn == 'technology' ? 'red' : ''
+                  }} >Technology</Button>
+
+
+                <Button onClick={() => handleSports('business')} className='m-2 w-100' variant="outline-dark"
+                  style={{
+                    backgroundColor: activeBtn == 'business' ? 'red' : ''
+                  }}>Business</Button>
+
+
+                <Button onClick={() => handleSports('politics')} className='m-2 w-100' variant="outline-dark"
+                  style={{
+                    backgroundColor: activeBtn == 'politics' ? 'red' : ''
+                  }}>Politics</Button>
+
+
+                <Button onClick={() => handleSports('entertainment')} className='m-2 w-100' variant="outline-dark"
+                  style={{
+                    backgroundColor: activeBtn == 'entertainment' ? 'red' : ''
+                  }}>Entartainment</Button>
+
+
+                <Button onClick={() => handleSports('science')} className='m-2 w-100' variant="outline-dark"
+                  style={{
+                    backgroundColor: activeBtn == 'science' ? 'red' : ''
+                  }}>science</Button>
+
+
               </div>
             </Col>
             <Col>
@@ -136,15 +178,15 @@ function App() {
                   news.length > 0 ?
                     news?.map(newsDay =>
                       <Col lg={4} sm={12} >
-                        <Card onClick={() => handleShow(newsDay)} className='m-2 border-light shadow border-3' style={{ width: '16rem', height: '30rem', backgroundColor:'#E5E1DA' }}>
+                        <Card onClick={() => handleShow(newsDay)} className='m-2 p-1 border-teritary shadow border-3' style={{ width: '18rem', height: '30rem', backgroundColor: '#E5E1DA' }}>
                           <Card.Img className='img-fluid' variant="top" src={newsDay.urlToImage} style={{ height: '200px' }} />
                           <Card.Body>
-                            <Card.Title style={{color:'#555352'}}>{newsDay.title}</Card.Title>
+                            <Card.Title style={{ color: '#555352' }}>{newsDay.title}</Card.Title>
                             <Card.Text>
                               {newsDay.publishedAt
                               }
                             </Card.Text>
-                            <Button variant="primary" className="me-2 mb-2" onClick={() => handleShow(newsDay)}>See More</Button>
+                            <Button variant="primary" className="me-2 mb-2 " onClick={() => handleShow(newsDay)}>See More</Button>
                           </Card.Body>
                         </Card>
 
@@ -170,10 +212,10 @@ function App() {
               <Modal.Body>
                 <h3>{activeNews.author}</h3> <br />
                 <h4>{activeNews.content}</h4>
-                
+
                 <p>{activeNews.description}</p>
               </Modal.Body>
-              
+
             </Modal>
 
           )
